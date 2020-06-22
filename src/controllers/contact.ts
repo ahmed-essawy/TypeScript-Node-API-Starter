a8624ddebe0b730233677a9460e2cad1b8142d3b
Ahmed Essawy <ahm.elessawy@gmail.com>
2020-06-22 16:14:41 +0200

    initial commit

--- src/controllers/contact.ts
+++ src/controllers/contact.ts
@@ -3,11 +3,11 @@ import { Request, Response } from "express";
 import { check, validationResult } from "express-validator";
 
 const transporter = nodemailer.createTransport({
-    service: "SendGrid",
-    auth: {
-        user: process.env.SENDGRID_USER,
-        pass: process.env.SENDGRID_PASSWORD
-    }
+  service: "SendGrid",
+  auth: {
+    user: process.env.SENDGRID_USER,
+    pass: process.env.SENDGRID_PASSWORD,
+  },
 });
 
 /**
@@ -15,40 +15,40 @@ const transporter = nodemailer.createTransport({
  * Contact form page.
  */
 export const getContact = (req: Request, res: Response) => {
-    res.render("contact", {
-        title: "Contact"
-    });
+  res.render("contact", {
+    title: "Contact",
+  });
 };
 
 /**
  * POST /contact
  * Send a contact form via Nodemailer.
  */
-export const postContact = (req: Request, res: Response) => {
-    check("name", "Name cannot be blank").not().isEmpty();
-    check("email", "Email is not valid").isEmail();
-    check("message", "Message cannot be blank").not().isEmpty();
+export const postContact = async (req: Request, res: Response) => {
+  await check("name", "Name cannot be blank").not().isEmpty().run(req);
+  await check("email", "Email is not valid").isEmail().run(req);
+  await check("message", "Message cannot be blank").not().isEmpty().run(req);
 
-    const errors = validationResult(req);
+  const errors = validationResult(req);
 
-    if (!errors.isEmpty()) {
-        req.flash("errors", errors.array());
-        return res.redirect("/contact");
+  if (!errors.isEmpty()) {
+    req.flash("errors", errors.array());
+    return res.redirect("/contact");
+  }
+
+  const mailOptions = {
+    to: "your@email.com",
+    from: `${req.body.name} <${req.body.email}>`,
+    subject: "Contact Form",
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (err) => {
+    if (err) {
+      req.flash("errors", { msg: err.message });
+      return res.redirect("/contact");
     }
-
-    const mailOptions = {
-        to: "your@email.com",
-        from: `${req.body.name} <${req.body.email}>`,
-        subject: "Contact Form",
-        text: req.body.message
-    };
-
-    transporter.sendMail(mailOptions, (err) => {
-        if (err) {
-            req.flash("errors", { msg: err.message });
-            return res.redirect("/contact");
-        }
-        req.flash("success", { msg: "Email has been sent successfully!" });
-        res.redirect("/contact");
-    });
+    req.flash("success", { msg: "Email has been sent successfully!" });
+    res.redirect("/contact");
+  });
 };
